3fa62a04e2b60db8eac2f833ba48f4120bc05d69
sarobinson2011 <sarobinson2011@gmail.com>
2022-12-31 12:40:25 +0000

    created FundMe.sol

---
+++ contracts/StorageFactory.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifer: MIT
+
+pragma solidity ^0.6.0;
+
+import "./SimpleStorage.sol";
+
+contract StorageFactory is SimpleStorage {
+
+    SimpleStorage[] public simpleStorageArray;
+
+// This function deploys a new contract from this contract
+
+    function createSimpleStorageContract() public {
+        SimpleStorage simpleStorage = new SimpleStorage();
+        simpleStorageArray.push(simpleStorage);
+    }
+
+// The following 2 functions allow us to interact with different contracts from outside those contracts 
+
+    function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber) public {      
+        // this line has an explicit cast to the address type and initialises a new SimpleStorage object from the address
+        SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).store(_simpleStorageNumber);
+    }
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns (uint256) {
+        // this line has an explicit cast to the address type and initialises a new SimpleStorage object from the address
+        return SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).retrieve();
+    }
+}
