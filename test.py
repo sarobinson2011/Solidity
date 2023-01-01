75bf2dd6878ac169177e6b5a63224f063e4e7ebd
sarobinson2001 <sarobinson2011@gmail.com>
2023-01-01 13:02:42 +0000

    a test python file invoking import os

---
+++ test.py
@@ -0,0 +1,27 @@
+import random as rd
+import os
+from time import sleep 
+
+# os.system('clear')
+
+class Solution():
+
+    def myFunc(self):
+        for i in range(5):
+            dice1 = rd.randint(1, 6)
+            print(f'dice #1 (roll-{i}) = {dice1}')
+
+    def clear(self):
+        sleep(5)
+        os.system('clear')
+
+
+sol = Solution()
+sol.clear()
+sol.myFunc()
+
+testvAR = rd.randbytes(22)
+print(f'\ntestvAR = {testvAR}\n')
+
+
+
