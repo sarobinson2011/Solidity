1a0b6c173f390415b7c6871073c655c243f201f8
sarobinson2001 <sarobinson2011@gmail.com>
2023-01-02 14:56:17 +0000

    updated FundMe.sol

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -2,7 +2,20 @@
 
 pragma solidity >=0.6.6 <0.9.0;
 
+    // get the latest ETH/USD price from the Chainlink price feed
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.6/vendor/SafeMathChainlink.sol";
+
 contract FundMe {
-    // code here = blah blah blah 
-    // added a comment to check if I can commit-->push from VScode
+    // safe math library check uint256 for integer over/under flows
+    using SafeMathChainlink for uint256;    
+
+    // mapping to store which addresses deposited how much ETH
+    mapping(address => uint256) public addressToAmountFunded;
+    
+    // array of addresses who deposited funds
+    address[] public funders;
+    
 }
+
